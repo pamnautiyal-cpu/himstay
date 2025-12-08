ec9d1780ed15a26be4f4026340beba812637141f
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2025-12-08 13:30:14 +0530

    fix api url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,14 +4,12 @@ const API = axios.create({
   baseURL: "https://himstay.onrender.com/api",
 });
 
-// Add token if user is logged in
+// Auth token add (if user logged in)
 API.interceptors.request.use((req) => {
   const user = localStorage.getItem("user");
-
   if (user) {
     req.headers.Authorization = `Bearer ${JSON.parse(user).token}`;
   }
-
   return req;
 });
 
