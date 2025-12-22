2770fb62569bb1a68b13b3672318ccf6ca679436
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2025-12-22 18:25:18 +0530

    Fix routes: correct Home component rendering

--- frontend/src/MainRoutes.jsx
+++ frontend/src/MainRoutes.jsx
@@ -1,64 +1,24 @@
 import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Hotels from "./pages/Hotels";
+import HotelDetails from "./pages/HotelDetails";
+import Booking from "./pages/Booking";
+import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Hotels from "./pages/Hotels";
-import Dashboard from "./pages/Dashboard";
-import MyTrips from "./pages/MyTrips";
-import AdminAddHotel from "./pages/AdminAddHotel";
-import HotelDetails from "./pages/HotelDetails";
-import Contact from "./pages/Contact";
-
-// 🔒 Protected Route
-function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
-  if (!token) return <Navigate to="/login" replace />;
-  return children;
-}
 
 function MainRoutes() {
   return (
     <Routes>
-      {/* Public */}
       <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<Hotels />} />
+      <Route path="/hotels/:id" element={<HotelDetails />} />
+      <Route path="/booking/:id" element={<Booking />} />
+      <Route path="/contact" element={<Contact />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/hotels" element={<Hotels />} />
-      <Route path="/hotel/:id" element={<HotelDetails />} />
-      <Route path="/contact" element={<Contact />} />
-
-      {/* Protected */}
-      <Route
-        path="/mytrips"
-        element={
-          <ProtectedRoute>
-            <MyTrips />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/admin/add-hotel"
-        element={
-          <ProtectedRoute>
-            <AdminAddHotel />
-          </ProtectedRoute>
-        }
-      />
-
-      {/* Fallback */}
-      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
