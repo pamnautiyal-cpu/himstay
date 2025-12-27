15c1ad8168d783a3dc358c2d4405600500eb86e7
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2025-12-27 20:19:59 +0530

    Fix YogaDetails filename to match route import

--- frontend/src/MainRoutes.jsx
+++ frontend/src/MainRoutes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import HotelDetails from "./pages/HotelDetails";
-import YogaDetails from "./pages/YogaDetails";
+import YogaDetails from "./pages/YogaDetail";
 import Booking from "./pages/Booking";
 
 export default function MainRoutes() {
@@ -11,7 +11,7 @@ export default function MainRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/hotel/:id" element={<HotelDetails />} />
-      <Route path="/yoga/:slug" element={<YogaDetails />} />
+      <Route path="/yoga/:slug" element={<YogaDetail />} />
       <Route path="/booking" element={<Booking />} />
     </Routes>
   );
