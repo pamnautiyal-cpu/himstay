4d48073c14682e93cc915d5b1eda85cfe679fb43
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2025-12-28 16:16:43 +0530

    Fix images using public folder for Render

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -42,18 +42,18 @@ export default function Home() {
         </p>
       </div>
 
-      {/* DESTINATIONS */}
+      {/* TOP DESTINATIONS */}
       <section style={section}>
         <h2>Top destinations in Uttarakhand</h2>
         <div style={grid}>
           {[
-            ["Mussoorie", "/assets/images/destinations/mussoorie.jpg"],
-            ["Nainital", "/assets/images/destinations/nainital.jpg"],
-            ["Dehradun", "/assets/images/destinations/dehradun.jpg"],
-            ["Haldwani", "/assets/images/destinations/haldwani.jpg"],
-          ].map(([name, src]) => (
+            ["Mussoorie", "/images/destinations/mussoorie.jpg"],
+            ["Nainital", "/images/destinations/nainital.jpg"],
+            ["Dehradun", "/images/destinations/dehradun.jpg"],
+            ["Haldwani", "/images/destinations/haldwani.jpg"],
+          ].map(([name, image]) => (
             <div key={name} style={card}>
-              <img src={src} alt={name} style={img} />
+              <img src={image} alt={name} style={img} />
               <div style={{ padding: 16 }}>{name}</div>
             </div>
           ))}
@@ -65,55 +65,48 @@ export default function Home() {
         <h2>Char Dham Yatra Destinations</h2>
         <div style={grid}>
           {[
-            ["Kedarnath", "/assets/images/chardham/kedarnath.jpg"],
-            ["Badrinath", "/assets/images/chardham/badrinath.jpg"],
-            ["Gangotri", "/assets/images/chardham/gangotri.jpg"],
-            ["Yamunotri", "/assets/images/chardham/yamunotri.jpg"],
-            ["Hemkund Sahib", "/assets/images/chardham/hemkund.jpg"],
-          ].map(([name, src]) => (
+            ["Kedarnath", "/images/chardham/kedarnath.jpg"],
+            ["Badrinath", "/images/chardham/badrinath.jpg"],
+            ["Gangotri", "/images/chardham/gangotri.jpg"],
+            ["Yamunotri", "/images/chardham/yamunotri.jpg"],
+            ["Hemkund Sahib", "/images/chardham/hemkund.jpg"],
+          ].map(([name, image]) => (
             <div key={name} style={card}>
-              <img src={src} alt={name} style={img} />
+              <img src={image} alt={name} style={img} />
               <div style={{ padding: 16 }}>{name}</div>
             </div>
           ))}
         </div>
       </section>
 
-      {/* TREKS */}
+      {/* TREKKING */}
       <section style={section}>
         <h2>Trekking & Adventure in Uttarakhand</h2>
         <div style={grid}>
-          {[
-            ["Kedarkantha Trek", "/treks/kedarkantha", "/assets/images/treks/kedarkantha.jpg"],
-            ["Har Ki Dun Trek", "/treks/har-ki-dun", "/assets/images/treks/har-ki-dun.jpg"],
-            ["Nag Tibba Trek", "/treks/nag-tibba", "/assets/images/treks/nag-tibba.jpg"],
-            ["Valley of Flowers", "/treks/valley-of-flowers", "/assets/images/treks/valley-of-flowers.jpg"],
-            ["Roopkund Trek", "/treks/roopkund", "/assets/images/treks/roopkund.jpg"],
-          ].map(([name, link, src]) => (
-            <Link key={name} to={link} style={card}>
-              <img src={src} alt={name} style={img} />
-              <div style={{ padding: 16 }}>{name}</div>
-            </Link>
-          ))}
-        </div>
-      </section>
+          <Link to="/treks/kedarkantha" style={card}>
+            <img src="/images/treks/kedarkantha.jpg" style={img} />
+            <div style={{ padding: 16 }}>Kedarkantha Trek</div>
+          </Link>
 
-      {/* YOGA */}
-      <section style={section}>
-        <h2>Yoga & Natural Therapy Retreats</h2>
-        <div style={grid}>
-          {[
-            ["Himalayan Yoga Retreat", "/yoga/himalayan-retreat", "/assets/images/yoga/himalayan-yoga.jpg"],
-            ["Naturopathy Healing", "/yoga/naturopathy", "/assets/images/yoga/naturopathy.jpg"],
-            ["Meditation & Pranayama", "/yoga/meditation", "/assets/images/yoga/meditation.jpg"],
-            ["Ayurvedic Therapy", "/yoga/ayurveda", "/assets/images/yoga/ayurveda.jpg"],
-            ["Panchakarma Detox", "/yoga/panchakarma", "/assets/images/yoga/panchakarma.jpg"],
-          ].map(([name, link, src]) => (
-            <Link key={name} to={link} style={card}>
-              <img src={src} alt={name} style={img} />
-              <div style={{ padding: 16 }}>{name}</div>
-            </Link>
-          ))}
+          <Link to="/treks/har-ki-dun" style={card}>
+            <img src="/images/treks/har-ki-dun.jpg" style={img} />
+            <div style={{ padding: 16 }}>Har Ki Dun Trek</div>
+          </Link>
+
+          <Link to="/treks/nag-tibba" style={card}>
+            <img src="/images/treks/nag-tibba.jpg" style={img} />
+            <div style={{ padding: 16 }}>Nag Tibba Trek</div>
+          </Link>
+
+          <Link to="/treks/valley-of-flowers" style={card}>
+            <img src="/images/treks/valley-of-flowers.jpg" style={img} />
+            <div style={{ padding: 16 }}>Valley of Flowers</div>
+          </Link>
+
+          <Link to="/treks/roopkund" style={card}>
+            <img src="/images/treks/roopkund.jpg" style={img} />
+            <div style={{ padding: 16 }}>Roopkund Trek</div>
+          </Link>
         </div>
       </section>
     </div>
