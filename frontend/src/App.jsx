53feaf402fc3a491dbc9a307680db02c4f067c19
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2026-06-09 12:48:18 +0530

    Fix: remove duplicate BrowserRouter to resolve blank screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import AllStays from "./pages/AllStays";
@@ -10,15 +10,13 @@ import AdminBookings from "./pages/AdminBookings";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hotels" element={<AllStays />} />
-        <Route path="/hotels/:id" element={<HotelDetails />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/mytrips" element={<MyTrips />} />
-        <Route path="/admin/bookings" element={<AdminBookings />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<AllStays />} />
+      <Route path="/hotels/:id" element={<HotelDetails />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/mytrips" element={<MyTrips />} />
+      <Route path="/admin/bookings" element={<AdminBookings />} />
+    </Routes>
   );
 }
