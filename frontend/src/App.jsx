bbab9854a06b1bfabea880846b5bf5cd97a47f20
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2026-06-10 17:30:27 +0530

    Fixed routing for HotelDetails and synced UI with payment logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-
-// Import all pages
 import Home from "./pages/Home";
 import AllStays from "./pages/AllStays";
-import MyTrips from "./pages/MyTrips";
-import Contact from "./pages/Contact";
-import ListProperty from "./pages/ListProperty";
+import HotelDetails from "./pages/HotelDetails";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
+import Contact from "./pages/Contact";
+import MyTrips from "./pages/MyTrips";
+import ListProperty from "./pages/ListProperty";
 import "./App.css";
 
 export default function App() {
@@ -19,11 +17,12 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<AllStays />} />
-        <Route path="/mytrips" element={<MyTrips />} />
+        <Route path="/hotels/:id" element={<HotelDetails />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/list-property" element={<ListProperty />} />
+        <Route path="/mytrips" element={<MyTrips />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/list-property" element={<ListProperty />} />
       </Routes>
     </Layout>
   );
