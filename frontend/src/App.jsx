2d7964fb90153d6149d782882da9fc47ec30df52
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2026-06-10 15:13:17 +0530

    Final: Route connectivity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+
+// Pages Import karo (Check kar lo ki folder path yahi hai)
 import Home from "./pages/Home";
-import "./App.css"; // CSS import zaruri hai!
+import AllStays from "./pages/AllStays";
+import MyTrips from "./pages/MyTrips";
+import Contact from "./pages/Contact";
+import ListProperty from "./pages/ListProperty";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Baki routes yahan add karte jao */}
+        <Route path="/hotels" element={<AllStays />} />
+        <Route path="/mytrips" element={<MyTrips />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/list-property" element={<ListProperty />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Layout>
   );
