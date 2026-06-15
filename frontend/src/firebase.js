d57d45bb4218637cafda669d95af4c224ba27a7c
pamnautiyal-cpu <pam.nautiyal@gmail.comq>
2026-06-16 00:50:19 +0530

    Fix firebase config

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,17 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDInqFYNEz1qSEjXqpGbPmKIQdqwHrICqU",
   authDomain: "thehimalayans-in.firebaseapp.com",
   projectId: "thehimalayans-in",
   storageBucket: "thehimalayans-in.appspot.com",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "1011898592154",
+  appId: "1:1011898592154:web:d053a25558430f870a6823",
+  measurementId: "G-3LCR1GSFJ0"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
